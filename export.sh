81acd3163132ff34ffa3ae0d13bbb1407b5c4177
hoaitt <thanhhoai2209@gmail.com>
2018-02-28 09:31:42 +0700

    Update export.sh

--- export.sh
+++ export.sh
@@ -3,5 +3,5 @@ os="`uname`"
 now=$(date +"%m_%d_%Y")
 file="./db_$now.sql"
 db_host_name=db
-docker-compose exec $db_host_name sh -c 'exec mysqldump "$MYSQL_DATABASE" -u"MYSQL_USER" -p"$MYSQL_PASSWORD"' > $file
+docker-compose exec $db_host_name sh -c 'export MYSQL_PWD="$MYSQL_PASSWORD"; exec mysqldump "$MYSQL_DATABASE" -u"$MYSQL_USER"' > $file
 gzip $file
